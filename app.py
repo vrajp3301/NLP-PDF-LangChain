8f472f46a60ef339bcdfd130d20485fc88c441a9
Vraj Patel <vrajp3301@gmail.com>
2023-07-07 15:09:09 +0530

    Adding function to handle user input and chat history

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from langchain.chat_models import ChatOpenAI
+from html_template import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
 
 def get_text_from_pdf(docs):
     """
@@ -39,7 +40,7 @@ def get_vector_store(split_text):
     pass
 
 def get_conversation_chain(vector_store):
-    llm = ChatOpenAI()
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
     memory = ConversationBufferMemory(memory_key='chat_history',return_messages=True)
     chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
@@ -48,16 +49,35 @@ def get_conversation_chain(vector_store):
     )
     return chain
 
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="NLP-PDFs-LangChain",page_icon=":notebook_with_decorative_cover:")
-    st.header("NLP-PDFs-LangChain :ledger:")
-    st.text_input("Ask questions to the PDFs")
+    st.write(css,unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
 
+    st.header("NLP-PDFs-LangChain :ledger:")
+    user_question = st.text_input("Ask questions to the PDFs")
+   
+    if user_question:
+        handle_userinput(user_question)
+
+    
     with st.sidebar:
         st.subheader("Documents")
         
