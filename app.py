0a736de78cf327d15d990b6a92921185f3680ac2
Vraj Patel <vrajp3301@gmail.com>
2023-07-07 14:09:27 +0530

    Adding Text Splitter function(LangChain)

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
-
+from langchain.text_splitter import CharacterTextSplitter
 
 def get_text_from_pdf(docs):
     """
@@ -14,6 +14,20 @@ def get_text_from_pdf(docs):
             text += page.extract_text()
     return text
 
+
+def get_text_chunk(text):
+    """
+    THIS FUNCTION SPLITS THE RAW TEXT INTO CHUNKS USING LANGCHAIN'S CHARACTER TEXT SPLITTER AND RETURNS CHUNKS"
+    """
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        # seperator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
 def main():
     load_dotenv()
@@ -29,8 +43,8 @@ def main():
         if st.button("Process the PDFs"):
             with st.spinner("Processing the PDFs"):
                 raw_text = get_text_from_pdf(docs)
-                st.write(raw_text)
-
+                # st.write(raw_text)
+                split_text = get_text_chunk(raw_text)
 
 if __name__=='__main__':
     main()
